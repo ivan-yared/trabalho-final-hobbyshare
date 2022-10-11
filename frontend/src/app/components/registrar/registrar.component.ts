1da4cec90b5df1706e7d8b2a697a6d8e4aaec7be
Ivan Yared <ivanyared@hotmail.com>
2022-10-11 19:20:16 -0300

    Criação do projeto Node e Angular (back e front)

---
+++ frontend/src/app/components/registrar/registrar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registrar',
+  templateUrl: './registrar.component.html',
+  styleUrls: ['./registrar.component.scss']
+})
+export class RegistrarComponent implements OnInit {
+
+  registrarForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.registrarForm = this.createFormGroup();
+
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(7)]),
+    })
+  }
+
+  registrar(): void {
+    console.log(this.registrarForm.value)
+  }
+
+}
