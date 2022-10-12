c35834eaa523e6b9fb8371783b7da996efd83fcd
Ivan Yared <ivanyared@hotmail.com>
2022-10-12 19:11:17 -0300

    Vídeo 5 concluído

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -14,6 +15,14 @@ exports.signup = async (req, res, next) => {
     const password = req.body.password;
 
     try {
+        const user = await User.find(email);
+        if (user[0].length > 0) {
+            const error = new Error("Usuário com este email já cadastrado.");
+            console.log("Usuário com esse email já cadastrado")
+            error.statusCode = 409;
+            throw error;
+        }
+
         const hashedPassword = await bcrypt.hash(password, 12);
         const userDetails = {
             name: name,
@@ -25,6 +34,44 @@ exports.signup = async (req, res, next) => {
 
         res.status(201).json({ message: `Usuário cadastrado!`});
 
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err)
+    }
+}
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try{
+        const user = await User.find(email);
+        if (user[0].length !== 1){
+            const error = new Error ('Usuário não encontrado.');
+            error.statusCode = 401;
+            throw error;
+        }
+        
+        const storedUser = user[0][0];
+        const isEqual = await bcrypt.compare(password, storedUser.password);
+
+        if (!isEqual) {
+            const error = new Error ('Senha incorreta.');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign(
+            {
+            email: storedUser.email,
+            userId: storedUser.id,
+            },
+            'secretfortoker',
+            { expiresIn: '1h' }
+        )
+        res.status(200).json({ token: token, userId: storedUser.id })
     } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500;
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(7)]),
+    })
+  }
+
+  login() {
+    this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe();
   }
 
 }
