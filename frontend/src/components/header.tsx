b167636f36a1f28b0fa9c57a7a9be700dd1554c2
Ivan Yared <ivanyared@hotmail.com>
2022-10-20 20:30:22 -0300

    Mudança das permissões no header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,19 +5,23 @@ export function Header () {
 
     function estaLogado () {
         if (user) {
-            
+           return (<>
+                <button type="button" className="login btn btn-outline-light my-5">Minha página</button>
+                <button type="button" className="login btn btn-outline-light my-5">Feed</button>
+                <button type="button" className="login btn btn-outline-light my-5">Logout</button>
+            </>)
         }
+        return (<>
+            <button type="button" className="login btn btn-outline-light my-5">Login</button>
+            <button type="button" className="registrar btn btn-outline-light my-5">Registrar</button>
+        </>)
     }
 
     return (
         <header className="cabecalho container-fluid">
       <div className="p-3 text-fade" data-aos="fade-up" data-aos-duration="1500">
           <h1>HobbyShare</h1>
-          <button type="button" className="login btn btn-outline-light my-5">Login</button>
-          <button type="button" className="registrar btn btn-outline-light my-5">Registrar</button>
-          <button type="button" className="login btn btn-outline-light my-5">Minha página</button>
-          <button type="button" className="login btn btn-outline-light my-5">Feed</button>
-          <button type="button" className="login btn btn-outline-light my-5">Logout</button>
+          {estaLogado()}
       </div>
     </header>
     )
